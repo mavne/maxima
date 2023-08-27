bb904afe8a8af19794271ecbda8f640e4db09983
giorgi <giorgigvazava87@gmail.com>
2023-08-27 14:39:08 +0400

    first commit

---
+++ _markup/index.php
@@ -0,0 +1,46 @@
+<?php 
+$version = "1.0.1";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="/_markup/assets/img/favicon.svg" type="image/svg" sizes="32x32">
+	<title>Maxima</title>
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/owl/owl.carousel.min.css" />
+	<link rel="stylesheet" href="/_markup/assets/lightbox/jquery.lightbox.css" />
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/main.css?v=<?=$version?>" />
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/aos/aos.css" />
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/ge.css?v=<?=$version?>" />
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/responsive_ge.css?v=<?=$version?>" />
+</head>
+<body>
+
+<section class="mobile-navigation"></section>
+
+<header>
+	
+</header>
+
+
+<main>
+	
+</main>
+
+<footer>
+
+</footer>
+
+
+
+<script type="text/javascript" src="/_markup/assets/js/jquery.js"></script>
+<script type="text/javascript" src="/_markup/assets/lightbox/jquery.lightbox.js"></script>
+<script type="text/javascript" src="/_markup/assets/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/_markup/assets/js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="/_markup/assets/js/main.js?v=<?=$version?>"></script>
+<script type="text/javascript" src="/_markup/assets/aos/aos.js"></script>
+</body>
+
+</html>
